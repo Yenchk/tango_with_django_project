657fb350774451ea8f701cd6d3b9e630d3cf69e4
Yanzhi Shu <2411021s@student.gla.ac.uk>
2021-02-03 21:10:30 +0800

    second commit

--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    response = []
+    response.append("Rango says hey there partner!")
+    url_about = "<a href='/rango/about/'>About</a>"
+    response.append(url_about)
+    return HttpResponse(response)
 
+def about(request):
+    response = []
+    url_index = "<a href='/rango/'>Index</a>"
+    response.append("Rango says here is the about page.")
+    response.append(url_index)
+    return HttpResponse(response)
