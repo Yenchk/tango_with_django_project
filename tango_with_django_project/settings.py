2102b398af786d426bc06fe5372b78fe0477e813
Yanzhi Shu <2411021s@student.gla.ac.uk>
2021-08-13 10:22:00 +0800

    key

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -22,10 +22,14 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k0e+)ja@^%e6iuwh)_9d0-44zeqyxuv$en1(h(@$)qm*%0e*f8'
+key = None
+with open('secret.key') as f:
+    key = f.read().strip()
+
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['Yenchk.pythonanywhere.com']
 
