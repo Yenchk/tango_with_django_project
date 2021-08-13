0c477d5cc231a00c2b3c4b5df98d5e85ed02ddca
Yanzhi Shu <2411021s@student.gla.ac.uk>
2021-08-13 16:14:45 +0800

    secret

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -23,7 +23,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 key = None
-with open('secret.key') as f:
+with open('../secret.key') as f:
     key = f.read().strip()
 
 SECRET_KEY = key
